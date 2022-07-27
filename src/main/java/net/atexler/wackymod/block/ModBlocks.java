d3c58807aa07f8390a24dbc6d061db85f17978a1
Alicja Texler <emunah73@gmail.com>
2022-07-26 21:07:24 -0400

    added custom stairs and slab

--- src/main/java/net/atexler/wackymod/block/ModBlocks.java
+++ src/main/java/net/atexler/wackymod/block/ModBlocks.java
@@ -10,6 +10,8 @@ import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.world.item.*;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SlabBlock;
+import net.minecraft.world.level.block.StairBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -34,6 +36,16 @@ public class ModBlocks {
     public static final RegistryObject<Block> CURSED_TITANIUM_ORE = registerBlock("cursed_titanium_ore",
             () -> new Block(BlockBehaviour.Properties.of(Material.STONE).strength(2f)
                     .requiresCorrectToolForDrops()), ModCreativeTab.WACKY_TAB);
+    public static final RegistryObject<Block> CURSED_TITANIUM_STAIRS = registerBlock("cursed_titanium_stairs",
+            () -> new StairBlock(() -> ModBlocks.CURSED_TITANIUM_BLOCK.get().defaultBlockState(),
+                        BlockBehaviour.Properties.of(Material.METAL).strength(5f)
+                        .requiresCorrectToolForDrops()), ModCreativeTab.WACKY_TAB);
+    public static final RegistryObject<Block> CURSED_TITANIUM_SLAB = registerBlock("cursed_titanium_slab",
+            () -> new SlabBlock(BlockBehaviour.Properties.of(Material.METAL).strength(5f)
+                    .requiresCorrectToolForDrops()), ModCreativeTab.WACKY_TAB);
+
+
+    //special blocks
     public static final RegistryObject<Block> BOUNCY_BLOCK = registerBlock("bouncy_block",
             () -> new BouncyBlock(BlockBehaviour.Properties.of(Material.GRASS).strength(2f)
                     .requiresCorrectToolForDrops()), ModCreativeTab.WACKY_TAB, "tooltip.wackymod.bouncy_block");
